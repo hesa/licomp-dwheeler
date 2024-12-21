8dc28d0bda9f2d69db5dd485b21917958c8f7857
Henrik Sandklef <hesa@sandklef.com>
2024-12-21 01:01:15 +0100

    add disclaimer from the origin page

--- licomp_dwheeler/config.py
+++ licomp_dwheeler/config.py
@@ -6,7 +6,7 @@ licomp_dwheeler_version = '0.3.2'
 my_supported_api_version = '0.3'
 
 repo = 'https://github.com/hesa/licomp-dwheeler/'
-disclaimer = 'The data or the output of the tools in this repository come with guarantee'
+disclaimer = 'This information is believed to be correct, but is not legal advice; for formal legal advice, please consult an attorney.'
 cli_name = 'licomp-dwheeler'
 module_name = 'licomp_dwheeler'
 
