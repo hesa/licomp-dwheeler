b65b61fd0fbc760b64304affae25e009b3e6af22
hesa <hesa@localhost>
2024-12-29 22:04:00 +0000

    Release 0.4.0

--- licomp_dwheeler/config.py
+++ licomp_dwheeler/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_dwheeler_version = '0.3.4'
+licomp_dwheeler_version = '0.4.0'
 my_supported_api_version = '0.4'
 
 repo = 'https://github.com/hesa/licomp-dwheeler/'
